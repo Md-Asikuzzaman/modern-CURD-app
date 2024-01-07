2aecd7b5741a49cde9588623f7f9a5322e67e5c9
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-01-07 11:32:40 +0600

    change some code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Card from './components/Card';
 
 export default async function Home() {
-  const res = await fetch('http://localhost:3000/api/posts', {
-    cache: 'no-cache',
-  });
-  const posts = await res.json();
+  // const res = await fetch('https://devasik.vercel.app/api/posts', {
+  //   cache: 'no-cache',
+  // });
+  // const posts = await res.json();
 
   return (
-    <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 py-10'>
-      {posts?.map((post: any) => (
-        <Card key={post.id} post={post} />
-      ))}
-    </main>
+    // <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 py-10'>
+    //   {posts?.map((post: any) => (
+    //     <Card key={post.id} post={post} />
+    //   ))}
+    // </main>
+
+    <div></div>
   );
 }
