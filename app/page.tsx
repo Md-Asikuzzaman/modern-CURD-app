652e947f262fceda1834d029082c30b8da8ebc3c
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-01-07 11:36:44 +0600

    add api endpoints

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
 import Card from './components/Card';
 
 export default async function Home() {
-  // const res = await fetch('https://devasik.vercel.app/api/posts', {
-  //   cache: 'no-cache',
-  // });
-  // const posts = await res.json();
+  const res = await fetch('https://modern-curd.vercel.app/api/posts', {
+    cache: 'no-cache',
+  });
+  const posts = await res.json();
 
   return (
-    // <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 py-10'>
-    //   {posts?.map((post: any) => (
-    //     <Card key={post.id} post={post} />
-    //   ))}
-    // </main>
-
-    <div></div>
+    <main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 py-10'>
+      {posts?.map((post: any) => (
+        <Card key={post.id} post={post} />
+      ))}
+    </main>
   );
 }
